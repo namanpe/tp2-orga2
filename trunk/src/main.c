0d71916f7828ba1f30acc27426d847d528e81467
matiasblanco18 <matiasblanco18@ac6710c6-b13e-0410-92b4-45230d45b441>
2007-11-29 17:55:15 +0000

    ordenando..

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -7,6 +7,35 @@ void realzarBordesRoberts(char*, int, int, char*, char*, char*);
 void umbralizar(char*, int, int, char*, char*, char*);
 void printHelp(char*);
 
+void roberts(){
+	printf("Ejecutando roberts...\n");
+}
+
+
+void prewitt(){
+	printf("Ejecutando prewitt...\n");
+}
+
+void sobel(){
+	printf("Ejecutando sobel...\n");
+}
+
+void umbralizarLauncher(){
+	printf("Ejecutando umbralizado...\n");
+}
+
+void monocromatizarPromedio(){
+	printf("Monocromatizando (Promedio)...\n");
+}
+
+void monocromatizarMaximo(){
+	printf("Monocromatizando (Maximo)...\n");
+}
+
+void displayHelp(){
+	printf("Ayuda... \n");
+}
+
 int main(int argc, char *argv[]){
 
 	if (argc < 2) {
@@ -20,7 +49,7 @@ int main(int argc, char *argv[]){
 		} else if (argv[1] == "r2") {
 			sobel();
 		} else if (argv[1] == "u") {
-			umbralizar();
+			umbralizarLauncher();
 		} else if (argv[1] == "m1") {
 			monocromatizarPromedio();
 		} else if (argv[1] == "m2") {
